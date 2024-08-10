e155185ad6f45a0f320f4817040e5119b49d5387
davi <ivad162@gmail.com>
2024-08-09 22:36:46 -0300

    bye

--- src/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import omz, ubuntu, p10k
-import os
-
-def take_config(home: str, assets: str) -> None:
-    os.rename(home + "/.zshrc", home + "/.zshrc.old")
-    os.symlink(assets: "/zshrc", home + "/.zshrc")
-    return
